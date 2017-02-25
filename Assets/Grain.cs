a88dad1c5ce00901a9c13dee9bc24088bf384c50
DESKTOP-O8MDLOJ\Salem <alemsalem@gmail.com>
2017-02-25 10:24:18 -0500

    I added the random Force and applied it to the grain. Random Force is not so random every run...

---
+++ Assets/Grain.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grain : MonoBehaviour {
+	private Rigidbody2D grainRB; // rigid body of the grain 
+
+	private float forceMuX = 0;   // average of the force in the x direction 
+	private float forceMuY = 0;  // average of the force in the y direction
+	private float forceSigma; // standard deviation of the force.
+
+	// Use this for initialization
+	void Start () {
+		forceSigma =10* Mathf.Sqrt (Time.fixedDeltaTime);
+		grainRB = GetComponent<Rigidbody2D> (); 
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		grainRB.AddForce (randomForceEta());
+
+		Vector2 grainPosition = transform.position;
+		if (grainPosition.x > 5.5f || grainPosition.y > 5.5f) {
+			transform.position = Vector2.zero;
+		}
+	}
+
+	/*********************************************************************************************
+	  return a normally distributed force with the correct average and standard deviation.
+      This uses the Box-Muller transform algorith, starting from two unifrom distributions.
+	**********************************************************************************************/
+	Vector2 randomForceEta() {
+		// First genereate the two uniformly distributed numbers.
+		Random.InitState(System.DateTime.Now.Millisecond);
+		float u1 = Random.Range(0.0000001f, 1.0f);
+		Random.InitState(System.DateTime.Now.Millisecond);
+		float u2 = Random.Range(0.0000001f, 1.0f);
+		// Transform from u1 and u2 to rescaled polar coordinates R, theta
+		float R = Mathf.Sqrt(-2 * Mathf.Log(u1));
+		float theta = 2 * Mathf.PI * u1;
+
+		// Transform to standard normally distributed x,y coordinates of the force
+		float Fx = R * Mathf.Cos (theta);
+		float Fy = R * Mathf.Sin (theta);
+
+		// Adjust to correct sigma and average.
+		Fx = forceSigma * Fx + forceMuX;
+		Fy = forceSigma * Fy + forceMuY;
+
+		// return the 2D force
+		return 10.0f *(new Vector2(Fx, Fy));
+	}
+}
---
+++ ReadME.txt
@@ -0,0 +1,10 @@
+Project Description:
+
+First Commit: 
+	I set up the project with two sprites for molecules and the grain. I added a random force script to the grain.
+	To get realistic motion I still need to tweek the parameters. Also the force is weird so far it doesn't
+	seem like it's random because different run all seem to be moving upwards. I will close and restart.
+
+Second Commit: 
+	before: I will try to fix the force and make it truely random. Then I will start putting realistic parameters
+	for average and standard deviation of the force.	
